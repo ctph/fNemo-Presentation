65780222476a58b58bc26a1e8c421056b152b472
rzpd <rizapradiptad@gmail.com>
2025-07-14 16:51:20 -0500

    Implemented navbar into people

---
+++ js/people.js
@@ -0,0 +1,14 @@
+console.log("People page loaded!");
+
+fetch("components/html/navbar.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("navbar-placeholder").innerHTML = html;
+
+    const path = window.location.pathname;
+    document.querySelectorAll(".nav-links a").forEach(link => {
+      if (path.includes(link.getAttribute("href"))) {
+        link.classList.add("active");
+      }
+    });
+  });
